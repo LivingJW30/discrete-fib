4a351ce8bc7407093d02e59749cd52509a3ae0dd
LivingJW30 <118578583+LivingJW30@users.noreply.github.com>
2024-03-28 17:01:25 -0400

    Fib function created

--- DiscreteFib.java
+++ DiscreteFib.java
@@ -1,5 +1,22 @@
 public class DiscreteFib{
     public static void main(String[] args){
-        System.out.println("Hello World");
+
+        int n = 40;
+
+        //for loop to print the return of our fibonacci function
+        for (int i=0;i<n;i++){
+            System.out.println(fib(i));
+
+            //do some code here to determine divisibility pattern
+        }
+    }
+
+    //function to print fibonacci sequence up to n
+    public static int fib(int n){
+        if (n<2){
+            return 1;
+        }
+
+        return fib(n-1)+fib(n-2);
     }
 }
